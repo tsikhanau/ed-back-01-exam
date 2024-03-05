80d8ca1d231d9ead7570188abf5e112abe885b12
aliaksandr.tsikhanau <aliaksandr.tsikhnanau@oomnitza.com>
2024-03-05 21:36:47 +0300

    -

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,37 @@
 import 'dotenv/config'
-import express, {Request, Response, NextFunction} from 'express'
+import express, {Request, Response} from 'express'
+import bodyParser from 'body-parser'
 
-// create express app
 const app = express()
 
-const port = process.env.PORT
+app.use(bodyParser.json())
 
-app.use('/', (req: Request, res: Response) => {
-    res.send("WORKS")
+app.get('/books', (req: Request, res: Response) => {
+    res.send(books)
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening on port: ${port}`)
+app.put('/books/:id', (req: Request, res: Response) => {
+    const book = books.find(b => b.id === +req.params.id)
+    if (book) {
+        book.title = req.body.title
+        res.send(book)
+    } else {
+        res.send(401)
+    }
 })
 
+app.listen(process.env.PORT, () => {
+    console.log(`Example app listening on port: ${process.env.PORT}`)
+})
+
+let books = [
+    {id: 1, title: 'Algorithms', year: 2020},
+    {id: 2, title: 'SQL', year: 2021},
+    {id: 3, title: 'CSS', year: 2019},
+    {id: 4, title: 'JS', year: 2018},
+    {id: 5, title: 'Back-end - Путь Самурая', year: 2022},
+]
+
+/*
+title почему-то исчезает после обновления. Исправьте баг и в качестве ответа напишите правильную версию 16 строки
+*/
